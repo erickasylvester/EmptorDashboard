0459097c9b517fd0c9e43ccd126dce41797ea7c0
Ericka Sylvester <sylvester.ericka@gmail.com>
2019-11-04 20:53:06 -0500

    feat(cleanup): remove debug statements and update readme

--- client/components/index.js
+++ client/components/index.js
@@ -1,7 +1,3 @@
 
 export {default as UserHome} from './user-home'
 export {default as DataDisplay} from './dataDisplay'
-// export {default as Candies} from './candies'
-// export {default as SelectedCandy} from './selected-candy'
-// export {default as Cart} from './cart-form'
-// export {default as OrderConfirmation} from './order-confirmation'
